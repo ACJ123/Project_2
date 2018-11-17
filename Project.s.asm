e1f16f46a5181a22af6841dc289b788bf18a5393
Amari Jones <flameaj@gmail.com>
2018-11-16 22:28:31 -0500

    Update Project.s.asm
    added the invalid input for future use and built in the exit case

--- Project.s.asm
+++ Project.s.asm
@@ -2,3 +2,9 @@
 .data
  emptyInput: .asciiz "Input is empty."
  longInput: .asciiz "Input is too long."
+  userInput: .space 500
+  invalidInput: .asciiz "Invalid base-N number." # change n
+  
+  .text
+  exit:
+  li $v0, 10
